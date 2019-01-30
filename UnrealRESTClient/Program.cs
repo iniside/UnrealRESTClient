400a552b9bf356f6488921da41bde0216147a98e
Lukasz 'iniside' Baran <iniside@live.com>
2019-01-30 11:31:15 +0100

    first commit

---
+++ UnrealRESTClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnrealRESTClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
